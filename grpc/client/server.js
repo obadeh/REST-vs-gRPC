f3addbf00f5a8cd4fd6a1a149d06b47db5da5297
obadeh <obadaquraan@yahoo.com>
2020-05-27 15:51:50 +0300

    grpc c&s, rest c&s

---
+++ grpc/client/server.js
@@ -0,0 +1,46 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+const PROTO_FILE = `${__dirname}/../protos/books.proto`;
+const SERVICE_B_PATH = process.env.SERVICE_B_PATH || "0.0.0.0:8080";
+
+const packageDefinition = protoLoader.loadSync(PROTO_FILE);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const Client = protoDescriptor.books.BooksService;
+const Books = new Client(SERVICE_B_PATH, grpc.credentials.createInsecure());
+// const REQUEST_COUNT = process.env.REQUEST_COUNT;
+const REQUEST_COUNT = 100000;
+
+const getEmptyAsync = call =>
+  new Promise((resolve, reject) => {
+    Books.getEmpty(call, (err, res) => (err ? reject(err) : resolve(res)));
+  });
+
+const getOneAsync = call =>
+  new Promise((resolve, reject) => {
+    Books.getOne(call, (err, res) => (err ? reject(err) : resolve(res)));
+  });
+
+const getAllAsync = call =>
+  new Promise((resolve, reject) => {
+    Books.getAll(call, (err, res) => (err ? reject(err) : resolve(res)));
+  });
+
+const benchmark = async f => {
+  const startTime = process.hrtime();
+  for (let i = 1; i <= REQUEST_COUNT; i++) {
+    await f(null);
+  }
+  const endTime = process.hrtime(startTime);
+  console.log(
+    `Done ${REQUEST_COUNT.toLocaleString()} ${f.name} in ${endTime[0]} s ${(
+      endTime[1] / 1e6
+    ).toFixed(4)} ms`
+  );
+};
+
+(async () => {
+  await benchmark(getEmptyAsync);
+  await benchmark(getOneAsync);
+  await benchmark(getAllAsync);
+})();
---
+++ rest/client/server.js
@@ -0,0 +1,35 @@
+const request = require("request");
+const http = require("http");
+
+http.globalAgent.keepAlive = true;
+
+// const REQUEST_COUNT = process.env.REQUEST_COUNT;
+const REQUEST_COUNT = 1000;
+const SERVICE_B_BASE_URL =
+  process.env.SERVICE_B_BASE_URL || "http://localhost:3001";
+
+const requestAsync = endpoint =>
+  new Promise((resolve, reject) => {
+    request(`${SERVICE_B_BASE_URL}${endpoint}`, (err, res, body) =>
+      err ? reject(err) : resolve(body)
+    );
+  });
+
+const benchmark = async endpoint => {
+  const startTime = process.hrtime();
+  for (let i = 1; i <= REQUEST_COUNT; i++) {
+    await requestAsync(endpoint);
+  }
+  const endTime = process.hrtime(startTime);
+  console.log(
+    `Done ${REQUEST_COUNT.toLocaleString()} ${endpoint} in ${endTime[0]} s ${(
+      endTime[1] / 1e6
+    ).toFixed(4)} ms`
+  );
+};
+
+(async () => {
+  await benchmark("/empty");
+  await benchmark("/books/1");
+  await benchmark("/books");
+})();
